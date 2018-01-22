075bc132472b68a5ac52d4ec26dc4178ee44eb37
bobandraa <sandra.cohen.ux@gmail.com>
2018-01-21 21:40:59 -0800

    changed data formant and increased data size

--- client/index.js
+++ client/index.js
@@ -3,9 +3,11 @@ document.getElementById('search').addEventListener('keyup', getText);
 function buildUrl (userQuery){
 	let api = "http://api.giphy.com/v1/gifs/search?";
 	let apiKey ="&api_key=Yzt6D7cNWlMRwMfklZWcERZvLk5Tnart";
-	let query = "&q=" + userQuery;
+	let query = "&q=" + userQuery + "&limit=50";
 	let url = api + apiKey + query;
+	console.log(url);
 	return url;
+
 };
 
 function getText (event){
@@ -22,10 +24,11 @@ function searchData(url) {
 	fetch(url)
 	.then(function(response){
 		return response.json();
+
 	})
 	.then(function(gifJson) {
 		for (i = 0; i < gifJson.data.length; i++){
-			let gifUrl = gifJson.data[i].images.original.url;
+			let gifUrl = gifJson.data[i].images.fixed_height_downsampled.url;
 			displayGif(gifUrl);
 		}
 	})
@@ -34,7 +37,7 @@ function searchData(url) {
 function displayGif(gifUrl) {
 	let imgTag = document.createElement("img");
 	imgTag.src = gifUrl;
-	let searchResultsDiv = document.getElementsByClassName("search-results");
+	let searchResultsDiv = document.getElementsByClassName("gif-box");
 
 	console.log (searchResultsDiv);
 	searchResultsDiv[0].appendChild(imgTag);
